a8461b3f12d9119b76fe7e422cfcce1c763a0595
SasanDilanthaSTD <sasandilantha12@gmail.com>
2025-12-05 00:54:34 +0530

    fix CustomException class issue

--- src/custom_exception.py
+++ src/custom_exception.py
@@ -1,17 +1,23 @@
-import traceback
 import sys
- 
+import traceback
+
 class CustomException(Exception):
-    def __init__(self, error_message, error_deatails:sys):
-         super().__init__(error_message)
-         self.error_message = self.get_detailed_error_message(error_message, error_deatails)
-    
-    @staticmethod    
-    def get_detailed_error_message(error_message, error_deatails: sys) -> str:
-        _, _, exc_tb = error_deatails.exc_info()
+    def __init__(self, error_message, error_details=None):
+        super().__init__(error_message)
+        self.error_message = self.get_detailed_error_message(error_message, error_details)
+
+    def get_detailed_error_message(self, error_message, error_details):
+        # Extract traceback from sys.exc_info(), not from error object
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+
+        # If traceback is not available
+        if exc_tb is None:
+            return f"{error_message}"
+
         file_name = exc_tb.tb_frame.f_code.co_filename
         line_number = exc_tb.tb_lineno
+
         return f"Error occurred in script: {file_name} at line number: {line_number} with message: {error_message}"
-    
+
     def __str__(self):
         return self.error_message
