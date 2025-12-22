47c81d3f36a38ba641fd2a35813211844d80d648
SasanDilanthaSTD <sasandilantha12@gmail.com>
2025-12-22 06:44:36 +0530

    fix: Fix the path miss match issue using `Path(__file__).resolve()`

--- config/path_config.py
+++ config/path_config.py
@@ -15,12 +15,17 @@ Attributes:
 """
 
 import os
+from pathlib import Path
 
-BASE_DIR = os.getcwd()
-MODEL_DIR = os.path.join(BASE_DIR, "models")
+# __file__ points to 'config/path_config.py'
+# .resolve() makes it an absolute path
+# .parent is the 'config' folder
+# .parent.parent is the 'ml-ops-project_1' folder (your project root)
+BASE_DIR = Path(__file__).resolve().parent.parent
+MODEL_DIR = BASE_DIR / "models"
 
-RAW_DIR = os.path.join(BASE_DIR, "artifacts/raw")
-RAW_DATA_PATH = os.path.join(RAW_DIR, "raw_data.csv")
-TRAIN_DATA_PATH = os.path.join(RAW_DIR, "train_data.csv")
-TEST_DATA_PATH = os.path.join(RAW_DIR, "test_data.csv")
-CONFIG_PATH = os.path.join(BASE_DIR, "config", "config.yml")
+RAW_DIR = BASE_DIR / Path("artifacts/raw")
+RAW_DATA_PATH = RAW_DIR / Path("raw_data.csv")
+TRAIN_DATA_PATH = RAW_DIR / Path("train_data.csv")
+TEST_DATA_PATH = RAW_DIR / Path("test_data.csv")
+CONFIG_PATH = BASE_DIR / Path("config/config.yml")
