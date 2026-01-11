11269ee32765b23fd28f1441daae404271298364
SasanDilanthaSTD <sasandilantha12@gmail.com>
2026-01-11 19:03:18 +0530

    feat: Complete the data processing stage

--- config/path_config.py
+++ config/path_config.py
@@ -29,3 +29,8 @@ RAW_DATA_PATH = RAW_DIR / Path("raw_data.csv")
 TRAIN_DATA_PATH = RAW_DIR / Path("train_data.csv")
 TEST_DATA_PATH = RAW_DIR / Path("test_data.csv")
 CONFIG_PATH = BASE_DIR / Path("config/config.yml")
+
+######################################## DATA PROCESSING PATHS ########################################
+DATA_PROCESSING_DIR = BASE_DIR / Path("artifacts/data_processing")
+PROCESSED_TRAIN_DATA_PATH = DATA_PROCESSING_DIR / Path("processed_train_data.csv")
+PROCESSED_TEST_DATA_PATH = DATA_PROCESSING_DIR / Path("processed_test_data.csv")
